b341d62267dbb0f49373695764297b6c0b087ae3
Okunaiya Daniel  Oluwatimilehin <161054981+OkunaiyaDanielOluwatimilehin@users.noreply.github.com>
2025-04-14 19:19:50 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -122,4 +122,18 @@ document.addEventListener("DOMContentLoaded", function () {
     resetButton.style.display = "none";
     backButton.style.display = "none";
   });   
+  //
+  // Testimonial slider functionality
+  const testimonials = document.querySelectorAll(".testimonial");
+let index = 0;
+
+function showNextTestimonial() {
+  testimonials[index].classList.remove("active");
+  index = (index + 1) % testimonials.length;
+  testimonials[index].classList.add("active");
+}
+
+setInterval(showNextTestimonial, 4000); // change every 4 seconds
+
 });
+
